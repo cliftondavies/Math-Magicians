61e8c96044e50f73df7dd648c7bfab440b4e7add
cliftondavies <clifton.davies@outlook.com>
2021-02-10 16:24:41 +0000

    Implement buttonGroup function with a default parameter

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,7 +3,7 @@ import Button from './Button';
 const ButtonPanel = () => {
   const renderButton = value => <Button buttonName={value} />;
 
-  const buttonGroup = (first, second, third, fourth) => (
+  const buttonGroup = (first, second, third, fourth = '') => (
     <div>
       {renderButton(first)}
       {renderButton(second)}
@@ -18,7 +18,7 @@ const ButtonPanel = () => {
       {buttonGroup('7', '8', '9', 'X')}
       {buttonGroup('4', '5', '6', '-')}
       {buttonGroup('1', '2', '3', '+')}
-      {buttonGroup('0', '.', '=', '')}
+      {buttonGroup('0', '.', '=')}
     </div>
   );
 };
