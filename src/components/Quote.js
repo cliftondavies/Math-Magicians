624e9517d20137dbf076e7cd413faeb753b0077d
cliftondavies <clifton.davies@outlook.com>
2021-02-17 18:06:19 +0000

    Update and style quote page

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,13 @@
-const Quote = () => (
-  <h2>
-    Quote
-  </h2>
-);
+const Quote = () => {
+  const paragraph = 'Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. – William Paul Thurston';
+
+  return (
+    <main className="quote">
+      <p>
+        {paragraph}
+      </p>
+    </main>
+  );
+};
 
 export default Quote;
