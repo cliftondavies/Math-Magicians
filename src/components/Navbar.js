3046e441cfd66dab9e30002c9642be0b970fffa8
cliftondavies <clifton.davies@outlook.com>
2021-02-17 17:07:40 +0000

    Add navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <header>
+  <header className="header">
     <h1>
       Math Magicians
     </h1>
+
     <nav>
       <Link to="/">Home </Link>
       <Link to="/calculator">Calculator</Link>
